84dc522b0e52d77d832ce6a7fec20454a708966d
장루빈집 <wkdfnqls2465@gmail.com>
2023-05-15 23:16:45 +0900

    위 조건 확인

--- jangrubin14.js
+++ jangrubin14.js
@@ -1,5 +1,7 @@
 function exampleOne(a) {
   if(typeof a ==='number') {
+    // ! a에서 소숫점을 제외한a를 뺐을 때 0이 아니라면 a는 실수
+    // console.log(1 - Math.floor(1.2))
     if(a - Math.floor(a) !== 0) {
       throw new Error('정수를 입력해야 합니다');
     }
@@ -8,3 +10,4 @@ function exampleOne(a) {
   }
   return a;
 }
+// exampleOne(1.2)
